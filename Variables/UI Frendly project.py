b322b7431b2415f113fc9e77ee47e526624c0400
Vimal Solanki <vimal.solanki@hotmail.co.uk>
2021-02-06 19:52:05 +0000

    Make an UI chat

---
+++ Variables/UI Frendly project.py
@@ -0,0 +1,14 @@
+import time
+def greet_user():
+    print('Welcome to my first UI connect portal!')
+    print("This is one of the first projects in Python. I" + "'" + "m just going to try a few things")
+name = input(" Hi, what is your name?")
+
+
+
+print("Hello," + name)
+greet_user()
+time.sleep(5)
+q1 = input("How can I help you today?")
+
+print("Thanks for talking " + name + " Stay safe!")
