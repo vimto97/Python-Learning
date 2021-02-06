da792731ae082d4f474a7f93353535318669113a
Vimal Solanki <vimal.solanki@hotmail.co.uk>
2021-02-06 20:38:13 +0000

    Recent project is an UI bot chat!

--- Variables/UI Frendly project.py
+++ Variables/UI Frendly project.py
@@ -1,13 +1,12 @@
 import time
-def greet_user():
-    print('Welcome to my first UI connect portal!')
+def greet_user(name):
+    print(f'Hello, {name}, Welcome to my first UI connect portal!')
     print("This is one of the first projects in Python. I" + "'" + "m just going to try a few things")
+
+
+
 name = input(" Hi, what is your name?")
-
-
-
-print("Hello," + name)
-greet_user()
+greet_user(name)
 time.sleep(5)
 q1 = input("How can I help you today?")
 
