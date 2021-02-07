5d8b27ed8ea0e81ba858848b6772c479f5c29723
Vimal Solanki <vimal.solanki@hotmail.co.uk>
2021-02-07 01:32:17 +0000

    Recent project is an UI bot chat!

--- Variables/UI Frendly project.py
+++ Variables/UI Frendly project.py
@@ -1,4 +1,5 @@
 import time
+#Fuctions
 def greet_user(name):
     print(f'Hello, {name}, Welcome to my first UI connect portal!')
     print("This is one of the first projects in Python. I" + "'" + "m just going to try a few things")
@@ -7,17 +8,22 @@ def ques(q1, name):
     print(f'{name} thanks for letting us know about {q1}')
 
 
-def book_event(event):
+#def book_event(event):
 
-
+#input
 name = input(" Hi, what is your name?")
+
+#function call
 greet_user(name)
+#timer
 time.sleep(5)
+#input
 q1 = input("What service would you like to book?")
 
-
+#function call
 ques(q1, name)
+#input
 event = input("Enter a date you like to get this done?")
 
-time.sleep(5)
-print("Thanks for talking I will get this booked for you " + name + "On the "+ event +" stay safe see you again.")
+#goodbye print message
+print("Thanks for talking I will get this booked for you " + name + "on the "+ event +", stay safe see you again.")
